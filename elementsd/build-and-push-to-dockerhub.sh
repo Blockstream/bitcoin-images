637974d2f251fde816c34bde6b165c1bf4ae8d1b
nitramiz <martin@blockstream.com>
2020-05-05 08:23:14 -0700

    update elementsd to 0.18.1.7

--- elementsd/build-and-push-to-dockerhub.sh
+++ elementsd/build-and-push-to-dockerhub.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-export VER=0.18.1.5
+export VER=0.18.1.7
 
 docker pull blockstream/elementsd:latest
 #docker build --cache-from blockstream/elementsd:latest -t blockstream/elementsd:${VER} . -f Dockerfile.gitian || { echo -e "\nSomething broke"; exit 1; }
 docker build --cache-from blockstream/elementsd:latest -t blockstream/elementsd:${VER} . || { echo -e "\nSomething broke"; exit 1; }
 docker push blockstream/elementsd:${VER}
 ## Uncomment to push :latest tag
-#docker tag blockstream/elementsd:${VER} blockstream/elementsd:latest
+docker tag blockstream/elementsd:${VER} blockstream/elementsd:latest
 docker push blockstream/elementsd:latest
 
 SHA=$(docker inspect --format='{{index .RepoDigests 0}}' blockstream/elementsd:${VER})
