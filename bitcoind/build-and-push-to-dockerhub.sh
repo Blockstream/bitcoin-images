d9171d9138ee62ce56b54b5c150f22d3f4392143
nitramiz <martin@blockstream.com>
2020-05-05 13:27:48 -0700

    update bitcoind + tor

--- bitcoind/build-and-push-to-dockerhub.sh
+++ bitcoind/build-and-push-to-dockerhub.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-export VER=0.19.0.1
+export VER=0.19.1
 
 docker pull blockstream/bitcoind:latest
 #docker build --cache-from blockstream/bitcoind:latest -t blockstream/bitcoind:${VER} -f Dockerfile.gitian . || { echo -e "\nSomething broke"; exit 1; }
 docker build --cache-from blockstream/bitcoind:latest -t blockstream/bitcoind:${VER} . || { echo -e "\nSomething broke"; exit 1; }
 docker push blockstream/bitcoind:${VER}
 ## Uncomment to push :latest tag
-#docker tag blockstream/bitcoind:${VER} blockstream/bitcoind:latest
-#docker push blockstream/bitcoind:latest
+docker tag blockstream/bitcoind:${VER} blockstream/bitcoind:latest
+docker push blockstream/bitcoind:latest
 
 SHA=$(docker inspect --format='{{index .RepoDigests 0}}' blockstream/bitcoind:${VER})
 
--- tor/build-and-push-to-dockerhub.sh
+++ tor/build-and-push-to-dockerhub.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+export VER=0.4.2.7
+
 docker pull blockstream/tor:latest
-docker build --cache-from blockstream/tor:latest -t blockstream/tor:latest . || `echo -e "\nSomething broke" && exit 1`
+docker build --cache-from blockstream/tor:latest -t blockstream/tor:${VER} . || { echo -e "\nSomething broke"; exit 1; }
+docker push blockstream/tor:${VER}
+docker tag blockstream/tor:${VER} blockstream/tor:latest
 docker push blockstream/tor:latest
 
 SHA=$(docker inspect --format='{{index .RepoDigests 0}}' blockstream/tor:latest)
