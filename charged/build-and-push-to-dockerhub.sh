31096dd3b80c1453406d24dc9ceffe0ddf7e79cd
nitramiz <martin@blockstream.com>
2020-06-23 05:41:24 -0700

    update charged

--- charged/build-and-push-to-dockerhub.sh
+++ charged/build-and-push-to-dockerhub.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+VERSION=v0.4.19
+
 docker pull blockstream/charged:latest
-docker build --cache-from blockstream/charged:latest -t blockstream/charged:latest . || `echo -e "\nSomething broke" && exit 1`
+docker build --cache-from blockstream/charged:latest -t blockstream/charged:$VERSION . || `echo -e "\nSomething broke" && exit 1`
+docker push blockstream/charged:$VERSION
+docker tag blockstream/charged:$VERSION blockstream/charged:latest
 docker push blockstream/charged:latest
 
 SHA=$(docker inspect --format='{{index .RepoDigests 0}}' blockstream/charged:latest)
