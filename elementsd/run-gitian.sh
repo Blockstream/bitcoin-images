b580c8b65ff26fd82163610379a34475bcf711d4
Chase Sillevis <chase@sillevis.net>
2021-10-29 18:52:15 +0200

    devops/tasks#1390: update elements to 0.21.0

--- elementsd/run-gitian.sh
+++ elementsd/run-gitian.sh
@@ -2,8 +2,8 @@
 
 set -v
 
-ELEMENTS_VERSION=elements-0.18.1.8
-ELEMENTS_DIR=0.18.1.8
+ELEMENTS_VERSION=elements-0.21.0
+ELEMENTS_DIR=0.21.0
 export USE_DOCKER=1
 
 git clone https://github.com/devrandom/gitian-builder.git gitian
