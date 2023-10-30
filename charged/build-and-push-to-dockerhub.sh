201cb3443e85ae613e100c5c53580af5f72909f0
nitramiz <martin@blockstream.com>
2023-10-30 12:41:37 -0100

    blockstream/charged:0.4.26

--- charged/build-and-push-to-dockerhub.sh
+++ charged/build-and-push-to-dockerhub.sh
@@ -1,12 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -ex
 
-VERSION=v0.4.24
+export VER=v0.4.26
 
 docker pull blockstream/charged:latest
-docker build --cache-from blockstream/charged:latest -t blockstream/charged:$VERSION . || `echo -e "\nSomething broke" && exit 1`
-docker push blockstream/charged:$VERSION
-docker tag blockstream/charged:$VERSION blockstream/charged:latest
-docker push blockstream/charged:latest
+docker build --network=host \
+  --build-arg CHARGED_VERSION=${VER} \
+  --cache-from blockstream/charged:latest \
+  -t blockstream/charged:${VER} . || { echo -e "\nSomething broke" && exit 1; }
+docker push blockstream/charged:${VER}
+
+if [[ $LATEST -eq 1 ]]
+then
+  docker tag blockstream/charged:${VER} blockstream/charged:latest
+  docker push blockstream/charged:latest
+fi
+
 
 SHA=$(docker inspect --format='{{index .RepoDigests 0}}' blockstream/charged:latest)
 
