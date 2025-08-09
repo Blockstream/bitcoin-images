77724fa1cb1da988f6fdf04cbf23863108d8e3d2
Chase Sillevis <chase@sillevis.net>
2025-08-09 09:32:54 +0200

    feat(waterfalls): switch to rcasetta repo and update commit

--- waterfalls/build-and-push-to-dockerhub.sh
+++ waterfalls/build-and-push-to-dockerhub.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -ex
 
-export VER=4abb0d99
+export WATERFALLS_REPO=https://github.com/RCasatta/waterfalls
+export WATERFALLS_COMMIT_HASH=4abb0d99e7c26d4c1b06d69fa4c574475a46ffbd
 
 docker buildx build \
     --platform linux/amd64,linux/arm64 \
     --push \
     --cache-from blockstream/waterfalls:latest \
-    --build-arg WATERFALLS_VERSION=${VER} \
-    -t blockstream/waterfalls:$VER \
+    --build-arg WATERFALLS_COMMIT_HASH=${WATERFALLS_COMMIT_HASH} \
+    -t blockstream/waterfalls:$WATERFALLS_COMMIT_HASH \
     -t blockstream/waterfalls:latest . || `echo -e "\nSomething broke" && exit 1`
+
+echo "pushed blockstream/waterfalls:$WATERFALLS_COMMIT_HASH"
+echo "pushed blockstream/waterfalls:latest"
+
+exit 0
