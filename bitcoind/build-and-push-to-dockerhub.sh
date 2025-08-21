34a25276964642fb829ab2b495c1d9d67f287df0
Chase Sillevis <chase@sillevis.net>
2025-08-21 18:30:40 +0200

    fix(bitcoin): make script posix compliant

--- bitcoind/build-and-push-to-dockerhub.sh
+++ bitcoind/build-and-push-to-dockerhub.sh
@@ -7,9 +7,8 @@ docker buildx build --platform linux/amd64,linux/arm64 \
   --cache-from blockstream/bitcoind:latest \
   --build-arg BITCOIN_VERSION=${VER} \
   --build-arg BUILDKIT_INLINE_CACHE=1 \
-  -t blockstream/bitcoind:${VER} . || { echo -e "\nSomething broke"; exit 1; }
+  -t blockstream/bitcoind:${VER} . || { printf '\nSomething broke\n'; exit 1; }
 
-if [[ $LATEST -eq 1 ]]
-then
+if [ "${LATEST:-0}" -eq 1 ]; then
   docker buildx imagetools create -t blockstream/bitcoind:latest blockstream/bitcoind:${VER}
 fi
